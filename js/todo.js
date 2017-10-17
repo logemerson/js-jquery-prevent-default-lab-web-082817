67c8b61428bc3adf652cb3a8aa68b234edff3dbc
github username <logemerson@gmail.com>
2017-10-17 15:22:06 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function() {
   // call functions here
+  addTask();
 });
 
 // define functions here
+function addTask() {
+  $("form").on("submit", function(ev) {
+    var task = $("#item").val();
+    $("ol").append("<li>" + task + "</li>");
+
+    ev.preventDefault();
+  });
+}
